049cd2ccf3b5f3e4e688a546ca65eed595d6ef88
Liam Cervante <liam.cervante@infracost.io>
2026-01-21 15:12:00 +0100

    test

--- main.tf
+++ main.tf
@@ -1,6 +1,90 @@
 
-provider "aws" {}
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "eastus"
+}
 
-resource "aws_s3_bucket" "bucket" {
-  name = "my-name"
+resource "azurerm_storage_account" "example" {
+  name                     = "examplesa"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "example_windows" {
+  name                = "example-windows-service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  os_type             = "Windows"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_service_plan" "example_linux" {
+  name                = "example-linux-service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_windows_function_app" "default" {
+  name                       = "example-windows-function-app"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  service_plan_id            = azurerm_service_plan.example_windows.id
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+
+  site_config {
+  }
+}
+
+resource "azurerm_linux_function_app" "example" {
+  name                       = "example-linux-function-app"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  service_plan_id            = azurerm_service_plan.example_linux.id
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+
+  site_config {}
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "example-app-service"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_service_plan.example_windows.id
+
+  site_config {}
+}
+
+resource "azurerm_function_app" "example" {
+  name                       = "example-legacy-function-app"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  app_service_plan_id        = azurerm_service_plan.example_windows.id
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+
+  site_config {}
+}
+
+resource "azurerm_windows_web_app" "example" {
+  name                = "example-windows-web-app"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  service_plan_id     = azurerm_service_plan.example_windows.id
+
+  site_config {}
+}
+
+resource "azurerm_linux_web_app" "example" {
+  name                = "example-linux-web-app"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  service_plan_id     = azurerm_service_plan.example_linux.id
+
+  site_config {}
 }
