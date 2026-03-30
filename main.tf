df8c3b9730d5837df83281f3564dd5e5ac68b137
Liam Cervante <liam.cervante@infracost.io>
2026-03-30 15:55:03 +0200

    feat: add aws_instance

--- main.tf
+++ main.tf
@@ -4,3 +4,8 @@ provider "aws" {}
 resource "aws_s3_bucket" "bucket" {
   name = "my-name"
 }
+
+resource "aws_instance" "what" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t3.micro"
+}
